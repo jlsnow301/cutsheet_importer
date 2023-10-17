49d1eb4a1f6350da6f1504da5ca1d825a1e875b3
jlsnow301 <jlsnow301@pm.me>
2023-10-16 21:40:01 -0700

    base64

--- auth_service.py
+++ auth_service.py
@@ -1,8 +1,6 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from requests import Request
+from google.oauth2.service_account import Credentials
 import os
-import pickle
 
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
@@ -10,38 +8,18 @@ SCOPES = [
 ]
 
 
-def get_credentials():
-    creds = None
+SERVICE_ACCOUNT_FILE = "credentials.json"
 
-    # Determine the path to credentials based on the environment
-    cred_path = (
-        "/home/runner/work/credentials.json"
-        if os.getenv("GITHUB_ACTIONS")
-        else "credentials.json"
-    )
 
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(cred_path, SCOPES)
-            creds = flow.run_local_server(port=0)
-
-            with open("token.pickle", "wb") as token:
-                pickle.dump(creds, token)
-
-    return creds
+def get_service_account_credentials():
+    return Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 
 def get_gmail_service():
-    creds = get_credentials()
+    creds = get_service_account_credentials()
     return build("gmail", "v1", credentials=creds)
 
 
 def get_calendar_service():
-    creds = get_credentials()
+    creds = get_service_account_credentials()
     return build("calendar", "v3", credentials=creds)
